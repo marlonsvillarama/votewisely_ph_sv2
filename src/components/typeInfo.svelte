6eded2afe6cf7d53ce38997bbfb4aee826277db6
Marlon Villarama <marlonsvillarama@gmail.com>
2024-12-23 21:01:23 +1300

    20241223

--- src/components/typeInfo.svelte
+++ src/components/typeInfo.svelte
@@ -2,9 +2,15 @@
     export let data = [];
 </script>
 
+<section class="buttons">
+    {#each data as d}
+    <button>{d.title}</button>
+    {/each}
+</section>
+
 {#each data as d}
-<section>
-    <h1>Responsibilities</h1>
+<section class="info hidden">
+    <h1>Responsibilities of a Senator</h1>
     <div>
         <p>A senator is an elected official of the Republic of the Philippines who is part of the upper house of Congress.</p>
         <p>Every 3 years, Filipinos must elect 12 of the 24 senators that make up the Senate of the Philippines. Each elected senator will serve six years in office, with a maximum of two consecutive terms.</p>
@@ -13,9 +19,27 @@
 {/each}
 
 <style>
-    section {
-        border: 1px solid rgba(0, 0, 0, 0.2);
-        /* border-radius: 0.25rem; */
+    .buttons {
+
     }
-    h1 {}
+    .info {
+        border: 1px solid rgba(0, 0, 0, 0.1);
+        /* border-radius: 0.25rem; */
+        margin-bottom: 1rem;
+    }
+    h1 {
+        background-color: rgba(0, 0, 0, 0.1);
+        padding: 0.5rem 1rem;
+        font-size: var(--fs-md);
+        color: var(--color-font);
+    }
+    div {
+        padding: 1rem 2rem;
+    }
+    p {
+        /* border: 1px solid red; */
+        /* padding: 0 1rem 0.5rem; */
+        line-height: 2rem;
+        /* text-indent: ; */
+    }
 </style>
